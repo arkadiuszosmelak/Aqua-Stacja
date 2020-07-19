db0c751eee3909bc73cd5d267f2fec854725b411
arkadiuszosmelak <arkadiuszosmelak@smcebi.edu.pl>
2020-07-19 21:47:56 +0200

    firstsss

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ def chart():
     for i in range (0, len(lista)):
         lista[i] = lista[i][0:-1]
     
-    lista = lista [-72:]
     odleglosci = lista[0::3]
     odleglosci = [round(float(i),2) for i in odleglosci]
     temperatury = lista[1::3]
@@ -32,9 +31,22 @@ def chart():
     akt_data = daty[-1]
     file.close()
 
-    
+    odleglosci1 = odleglosci[-24:]
+    odleglosci2 = odleglosci[-144:]
+    odleglosci3 = odleglosci[-288:]
 
-    return render_template('index.html', values = odleglosci, labels = daty, legend = legenda, tmp = temperatury, akt_odl = akt_odl, akt_tmp = akt_tmp, akt_data = akt_data)
+    print(odleglosci1, odleglosci2, odleglosci3)
+    temperatury1 = temperatury[-24:]
+    temperatury2 = temperatury[-144:]
+    temperatury3 = temperatury[-288:]
+
+    daty1 = daty[-24:]
+    daty2 = daty[-144:]
+    daty3 = daty[-288:]
+
+    # return render_template('index.html', odleglosci1 = odleglosci, labels1 = daty, legend = legenda, tmp1 = temperatury,  akt_odl = akt_odl, akt_tmp = akt_tmp, akt_data = akt_data)
+    # return render_template ('index.html')
+    return render_template('index.html', odleglosci1 = odleglosci1, odleglosci2 = odleglosci2, odleglosci3 = odleglosci3, labels1 = daty1, labels2 = daty2, labels3 = daty3, legend = legenda, tmp1 = temperatury1, tmp2 = temperatury2, tmp3 = temperatury3, akt_odl = akt_odl, akt_tmp = akt_tmp, akt_data = akt_data)
 
 
 if __name__ == "__main__":
--- randomxd.py
+++ randomxd.py
@@ -1,16 +1,13 @@
 import random
+import datetime
 
 file = open("testfile.txt", "a") 
-rok = 2020
-miesiac = 1
-dzien = 30
-godzina = 0
-minuta = 0
-sekundy = 0
-for i in range (864):
+for i in range (5, 289*5, 5):
     y = random.uniform(0.0,40.0)
     file.write(str(y)+"\n")
     x = random.uniform(10.0, 40.0)
     file.write(str(x)+"\n")
-    file.write("{0}-{1}-{2} {3}:{4}:{5}\n".format(2020,random.randint(1,12),random.randint(1,30),random.randint(0,23),random.randint(0,59),random.randint(0,59)))
+    data = datetime.datetime.now() - datetime.timedelta(minutes=i)
+    data = str(data)[:-7]
+    file.write(data+"\n")
 file.close()
